5577cca9b20dd35fe9e1a7ffd60c4a06c29b35fd
Anuja Bawaskar <anujabawaskar@gmail.com>
2018-07-03 17:17:46 +0530

    patient management REST web services
    json and xml support for performing CRUD operations on patient management systems

---
+++ restws/src/main/java/com/anuja/restws/model/Patient.java
@@ -0,0 +1,26 @@
+package com.anuja.restws.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="Patient")	//class can be serialized and de-serialized
+public class Patient {
+	private long id;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
